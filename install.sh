fe4cdb1fe969cadb783e06475ca22787ccc6742b
noneSycamore <sycamore.none@gmail.com>
2025-01-21 15:19:37 +0800

    Add install.sh for replacements in theme files

---
+++ install.sh
@@ -0,0 +1,3 @@
+sed -i '' 's/10/5/g' ./node_modules/hexo-theme-shokax/layout/_mixin/widgets.pug
+sed -i '' 's/10/5/g' ./node_modules/hexo-theme-shokax/source/js/_app/components/comments.ts
+sed -i '' 's/10/5/g' ./node_modules/hexo-theme-shokax/source/js/_app/components/tcomments.ts
